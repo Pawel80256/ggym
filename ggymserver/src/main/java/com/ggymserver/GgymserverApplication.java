e640f9411b2352e7daddbf6d618beef898c159af
pnowacki <paweuq2@yahoo.pl>
2024-09-08 13:05:30 +0200

    initial commit

---
+++ ggymserver/src/main/java/com/ggymserver/GgymserverApplication.java
@@ -0,0 +1,13 @@
+package com.ggymserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgymserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgymserverApplication.class, args);
+	}
+
+}
---
+++ ggymserver/src/test/java/com/ggymserver/GgymserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggymserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgymserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
