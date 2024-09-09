59057d21df4fa8fdb822890a560dfc7a35c03d47
pnowacki <paweuq2@yahoo.pl>
2024-09-09 17:15:38 +0200

    feat(database): added new tables, populated constant values

--- ggymserver/src/main/resources/database/2024-09-08/02-populate-cv-tables.sql
+++ ggymserver/src/main/resources/database/2024-09-08/02-populate-cv-tables.sql
@@ -2,10 +2,26 @@
 
 --changeset pnowacki:1
 insert into cv_muscle_part (value)
-values ('CHEST'),
-       ('BICEPS');
+values ('CALF'),
+       ('QUAD'),
+       ('HAMSTRING'),
+       ('GLUTES'),
+       ('ABS'),
+       ('BACK_EXTENSORS'),
+       ('LATS'),
+       ('CHEST'),
+       ('FRONT_DELT'),
+       ('SIDE_DELT'),
+       ('REAR_DELT'),
+       ('BICEPS'),
+       ('TRICEPS'),
+       ('FOREARM');
 
 --changeset pnowacki:2
 insert into cv_resistance_tool (value)
 values ('DUMBBELL'),
-       ('BARBELL');
+       ('BARBELL'),
+       ('DOUBLE_BARBELLS'),
+       ('RESISTANCE_BAND'),
+       ('WEIGHT_BELT'),
+       ('NONE');
